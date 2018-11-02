835f0a4e198d84b7f47c7e9ef103a7035e8d0600
Andrei Grigorev <angrigor@microsoft.com>
2018-11-01 18:30:52 -0700

    DI and SSL setup.

--- src/NuGet.Licenses/Global.asax.cs
+++ src/NuGet.Licenses/Global.asax.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Autofac;
+using Autofac.Integration.Mvc;
 using Microsoft.ApplicationInsights.Extensibility;
+using System.Net;
 using System.Web;
 using System.Web.Configuration;
 using System.Web.Mvc;
@@ -14,12 +17,34 @@ namespace NuGet.Licenses
     {
         protected void Application_Start()
         {
-            TelemetryConfiguration.Active.InstrumentationKey = WebConfigurationManager.AppSettings["Licenses.InstrumentationKey"];
+            SetupInstrumentation();
+            SetupSsl();
+            SetupDi();
 
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private static void SetupInstrumentation()
+        {
+            TelemetryConfiguration.Active.InstrumentationKey = WebConfigurationManager.AppSettings["Licenses.InstrumentationKey"];
+        }
+
+        private static void SetupSsl()
+        {
+            // Ensure that SSLv3 is disabled and that Tls v1.2 is enabled.
+            ServicePointManager.SecurityProtocol &= ~SecurityProtocolType.Ssl3;
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
+        }
+
+        private static void SetupDi()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
 }
