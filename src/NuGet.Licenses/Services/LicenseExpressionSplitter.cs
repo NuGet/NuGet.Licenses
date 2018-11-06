b499dc358b8b7ef0cd2e282409970fd6ed210e78
Andrei Grigorev <angrigor@microsoft.com>
2018-11-06 15:09:17 -0800

    File headers. Organized usings.

--- src/NuGet.Licenses/Services/LicenseExpressionSplitter.cs
+++ src/NuGet.Licenses/Services/LicenseExpressionSplitter.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using NuGet.Licenses.Models;
 using NuGet.Packaging.Licenses;
 
@@ -75,7 +76,6 @@ namespace NuGet.Licenses.Services
 
             return fullRunList;
         }
-
 
         private static void InOrderTraversal(NuGetLicenseExpression root, List<ComplexLicenseExpressionRun> runList)
         {
