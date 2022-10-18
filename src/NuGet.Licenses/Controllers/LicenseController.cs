b2365383e351dc1ceeec9f71c7505fd8b18dd7d1
zhhyu <Zhaohua.Yu@microsoft.com>
2022-10-18 14:24:34 -0700

    version 1.0

--- src/NuGet.Licenses/Controllers/LicenseController.cs
+++ src/NuGet.Licenses/Controllers/LicenseController.cs
@@ -144,7 +144,7 @@ namespace NuGet.Licenses.Controllers
                 {
                     Response.Cache.SetCacheability(System.Web.HttpCacheability.Public);
                     Response.Cache.SetMaxAge(new TimeSpan(0, 0, 0));
-                    Response.Cache.SetETag("Test-StaleWhileRevalidate");
+                    Response.Cache.SetETag("Version-1.0");
                 }
 
                 string licenseContent = _licenseFileService.GetLicenseFileContent(identifier);
